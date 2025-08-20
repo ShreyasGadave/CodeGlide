e5dfb7e1b99e58d88fb0aaf361b46e01f3fe0fd3
Shreyas Gadave <shreyasgadave777@gmail.com>
2025-08-20 18:34:22 +0530

    Delete Backend directory

--- Backend/Routes/roadmapRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { generateRoadmap } from "../Controller/roadmapControllers.js";
-
-
-const router = express.Router();
-
-router.post("/roadmap",generateRoadmap);
-
-export default router;
